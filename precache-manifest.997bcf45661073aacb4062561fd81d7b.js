c1468b6c90d25fe649affb3d0e63e141266ef97f
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-11-11 12:02:54 -0200

    Add build

---
+++ precache-manifest.997bcf45661073aacb4062561fd81d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96951908f5131b45606",
+    "url": "/Server/static/js/runtime~main.e9695190.js"
+  },
+  {
+    "revision": "2ac2cf4e6399eebdb71d",
+    "url": "/Server/static/js/main.2ac2cf4e.chunk.js"
+  },
+  {
+    "revision": "93a501a908b3b8161b4d",
+    "url": "/Server/static/js/1.93a501a9.chunk.js"
+  },
+  {
+    "revision": "2ac2cf4e6399eebdb71d",
+    "url": "/Server/static/css/main.3247842d.chunk.css"
+  },
+  {
+    "revision": "bb0137cfee7658f40d3a54e56f860fc0",
+    "url": "/Server/index.html"
+  }
+];
